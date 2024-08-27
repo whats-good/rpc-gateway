06ff8d10e0141be52b1b7d94340e47631afb3d18
Kerem Kazan <kerem.kazan@gmail.com>
2024-08-27 04:53:44 -0400

    added explicit constructor to chain

--- src/chain.rs
+++ src/chain.rs
@@ -1,7 +1,5 @@
-use std::time::Duration;
-
-use derive_more::derive::Constructor;
 use serde::Deserialize;
+use std::time::Duration;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug, Deserialize)]
 pub struct ChainId(u64);
@@ -12,13 +10,20 @@ impl From<u64> for ChainId {
     }
 }
 
-#[derive(PartialEq, Eq, Hash, Clone, Constructor, Debug, Deserialize)]
+#[derive(PartialEq, Eq, Hash, Clone, Debug, Deserialize)]
 pub struct Chain {
     pub block_time: Option<Duration>,
     pub chain_id: ChainId,
 }
 
 impl Chain {
+    pub fn new(chain_id: ChainId, block_time: Option<Duration>) -> Self {
+        Self {
+            chain_id,
+            block_time,
+        }
+    }
+
     pub fn leak(self) -> &'static Self {
         Box::leak(Box::new(self))
     }
