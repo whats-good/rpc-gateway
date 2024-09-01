aa6401b102267b44df0a355d9231880542f9e636
Kerem Kazan <kerem.kazan@gmail.com>
2024-09-01 13:45:26 -0400

    better logging

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,12 @@
 use std::io;
-use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter};
+use tracing_subscriber::{
+    fmt::{self},
+    layer::SubscriberExt,
+    EnvFilter,
+};
 
 pub fn setup_logging() {
+    // let log_file = File::create("kerem.log").expect("could not create log file");
     let subscriber = tracing_subscriber::registry()
         .with(EnvFilter::from_default_env())
         .with(
@@ -9,7 +14,12 @@ pub fn setup_logging() {
                 // .without_time()
                 .compact()
                 .with_ansi(true)
-                .with_writer(io::stdout),
+                // .with_ansi(false)
+                .with_file(true)
+                // .with_thread_ids(true) // TODO: is this useful?
+                .with_writer(io::stdout)
+                // .with_writer(log_file)
+                .with_line_number(true),
         );
     tracing::subscriber::set_global_default(subscriber).expect("Unable to set a global subscriber");
 }
